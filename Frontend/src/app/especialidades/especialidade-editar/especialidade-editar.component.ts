37385929590bb79a2bcab632c51da6bc146cc29a
Andressa Helena <andressa.h.moreira@avanade.com>
2022-12-21 15:27:40 -0300

    implementando editar na tela especialidade

--- Frontend/src/app/especialidades/especialidade-editar/especialidade-editar.component.ts
+++ Frontend/src/app/especialidades/especialidade-editar/especialidade-editar.component.ts
@@ -48,7 +48,7 @@ export class EspecialidadeEditarComponent {
       this.especialidade
     )
     .subscribe((data) => {
-      this.router.navigate(['especialidade-lista']);
+      this.router.navigate(['listarEspecialidades']);
       this.mensagemErro = false;
       this.mensagemSucesso = true;
     })
