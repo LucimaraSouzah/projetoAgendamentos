368533226bb354fed541d1fe3e25b4dec90ce08b
Ruana Rodrigues <ruana.rodrigues0@gmail.com>
2022-12-22 13:48:07 -0300

    resolve erro edite Hospital

--- Frontend/src/app/hospitais/hospital-editar/hospital-editar.component.ts
+++ Frontend/src/app/hospitais/hospital-editar/hospital-editar.component.ts
@@ -1,65 +1,35 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-
-import { IHospitalDto } from './../../interfaces/IHospitalDto';
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hospital-editar',
-  templateUrl: './hospital-editar.component.html',
-  styleUrls: ['./hospital-editar.component.css']
-})
+import { ActivatedRoute, Router } from '@angular/router';import { HttpClient } from '@angular/common/http';import { IHospitalDto } from './../../interfaces/IHospitalDto';import { Component } from '@angular/core';@Component({
+  selector: 'app-hospital-editar',  templateUrl: './hospital-editar.component.html',  styleUrls: ['./hospital-editar.component.css']})
 export class EditarHospitalComponent {
-[x: string]: any;
   hospital!: IHospitalDto;
   idHospital!: number;
-  route: any;
-
   mensagemSucesso = false;
   mensagemErro = false;
-
-  constructor(
-  mensagemSucesso = false;
-  mensagemErro = false;
-
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute,
-    private router: Router
-
-  ) {
+    private router: Router  ) {
     this.route.paramMap.subscribe((params) => {
-      this.idHospital = Number(params.get('id'));
-    });
+      this.idHospital = Number(params.get('id'));    });
   }
-
-  gOnInit(): void {
+  ngOnInit(): void {
     this.hospital = {
       idHospital: this.idHospital ?? 0,
-      nome: '',cnpj: '',
-      endereco: '',
-      telefone: '',
-      cnes: '',
-      ativo:false,
+      nome: '',      cnpj: '',      endereco: '',      telefone: '',      cnes: '',      ativo:false,
     };
-
     if (this.idHospital) {
-      this.http
-      .get(`https://localhost:7206/api/Hospital/${this.idHospital}`)
+      this.http      .get(`https://localhost:7206/api/Hospital/${this.idHospital}`)
       .subscribe((data) => {
           this.hospital = data as IHospitalDto;
       });
     }
   }
-
   editarHospital(id:number) {
       this.http.put(
         `https://localhost:7206/api/Hospital/${id}`,
-        this.hospital
-    )
+        this.hospital    )
     .subscribe((data) => {
-      this.router.navigate(['listaHospital']);
-      this.mensagemErro = false;
+      this.router.navigate(['listaHospital']);      this.mensagemErro = false;
       this.mensagemSucesso = true;
     }
   )}
